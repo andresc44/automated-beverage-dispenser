6e01116b1592fdf3c2c9f01ca0a04c8b4e714791
selinertan23 <66210295+selinertan23@users.noreply.github.com>
2023-04-02 14:59:12 -0400

    Update LCD.h
    lcdClear is just one line so we don't need a function for that

--- beverageDispenser/LCD.h
+++ beverageDispenser/LCD.h
@@ -5,6 +5,5 @@ void lcdSetup();                      // Setup LCD display
 void lcdPrint(String message);        // Print a given message on LCD, check length of message as well
 void lcdChoice(uint8_t choice);       // Print from default messages about the choice of drink
 void lcdPouring(uint8_t pump_number); // Print "Currently pouring XX"
-void lcdClear();                      // Clears Display
 
 #endif
