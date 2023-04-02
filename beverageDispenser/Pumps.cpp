7ef5549a48958e35206383504f3039c7950592fd
AdamCavallo <123097786+AdamCavallo@users.noreply.github.com>
2023-04-02 19:54:30 -0400

    Update Pumps.cpp

--- beverageDispenser/Pumps.cpp
+++ beverageDispenser/Pumps.cpp
@@ -1,1 +1,59 @@
 
+////Variables
+
+//Pump1
+int pump1_in1 = ;
+int pump1_in2 = ;
+
+//Pump2
+int pump2_in3 = ;
+int pump2_in4 = ;  
+
+//Pump3
+int pump3_in1 = ;
+int pump3_in2 = ;
+
+//Pump4
+int pump4_in3 = ;
+int pump4_in4 = ;
+
+
+void pumpsSetup() { // Setup pumps
+
+//Pump1
+pinMode(pump1_in1, OUTPUT);
+pinMode(pump1_in2, OUTPUT);
+
+//Pump2
+pinMode(pump2_in3, OUTPUT);
+pinMode(pump2_in4, OUTPUT);
+
+//Pump3
+pinMode(pump3_in1, OUTPUT);
+pinMode(pump3_in2, OUTPUT);
+
+//Pump4
+pinMode(pump4_in3, OUTPUT);
+pinMode(pump4_in4, OUTPUT);
+
+}
+
+
+
+void pumpDurations(uint8_t choice, int *durations[], uint8_t number_of_pumps){ // Alter pointer of durations depending on choice. Talk to Andres
+  
+  
+  
+}
+
+
+
+
+
+void pumpOn(uint8_t pump_number, int duration); {
+
+  
+}
+  
+
+
